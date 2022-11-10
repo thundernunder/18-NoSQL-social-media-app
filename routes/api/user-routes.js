2c1e186d4f61ad583e63cdc9b26d072cca4278aa
Nunder <nathannunderwood@gmail.com>
2022-11-09 20:11:02 -0800

    build user-routes - still need route to display user friends

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
 // need to add controllers endpoint to all of these variables 
-const getUsers = require('../../
-const getOneUser;
-const addUser;
-const updateUser;
-const deleteUser;
+const getUsers = require('../../controllers/user-controller');
 
-const addFriend;
-const deleteFriend;
+const getOneUser = require('../../controllers/user-controller');
+
+const addUser = require('../../controllers/user-controller');
+
+const updateUser = require('../../controllers/user-controller');
+
+const deleteUser = require('../../controllers/user-controller');
+
+
+const addFriend = require('../../controllers/user-controller');
+const deleteFriend = require('../../controllers/user-controller');
+
+router.route("/").get(getUsers).post(addUser);
+
+router.route("/:userID").get(getOneUser).put(updateUser);
+
+// need route for adding user friends
