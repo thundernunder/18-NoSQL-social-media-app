6f1c0e7341bff12871be619173321f15620a7e8a
Nunder <nathannunderwood@gmail.com>
2022-11-09 20:15:35 -0800

    build thought-routes and need route for handling thought reactions

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+// need to add controllers endpoint to all of these variables 
+const getThoughts = require('../../controllers/thought-controller');
+
+const getOneThought = require('../../controllers/thought-controller');
+
+const addThought = require('../../controllers/thought-controller');
+
+const updateThought = require('../../controllers/thought-controller');
+
+const deleteThought = require('../../controllers/thought-controller');
+
+
+const addReaction = require('../../controllers/thought-controller');
+const deleteReaction = require('../../controllers/thought-controller');
+
+router.route("/").get(getThoughts).post(addThought);
+
+router.route("/:thoughtID").get(getOneThought).put(updateThought);
+
+// need route for adding thought reactions
