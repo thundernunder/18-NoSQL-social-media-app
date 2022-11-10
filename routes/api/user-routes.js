962f743d6de10a746c68ebd7d7a90c8a04b49170
Nunder <nathannunderwood@gmail.com>
2022-11-09 20:00:28 -0800

    continue routes and build controller folder to establish user-route endpoint

---
+++ routes/api/user-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+// need to add controllers endpoint to all of these variables 
+const getUsers = require('../../
+const getOneUser;
+const addUser;
+const updateUser;
+const deleteUser;
+
+const addFriend;
+const deleteFriend;
