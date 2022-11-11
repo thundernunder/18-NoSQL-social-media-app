154355a08cc708802b0a556e872b4f95c08f43fa
Nunder <nathannunderwood@gmail.com>
2022-11-10 22:38:51 -0800

    change structure of method calling for user-routes to fix errors. starting for thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,18 +1,27 @@
 const router = require('express').Router();
 // need to add controllers endpoint to all of these variables 
-const getThoughts = require('../../controllers/thought-controller');
+const {
+    getThoughts, 
+    getOneThought, 
+    addThought,
+    updateThought,
+    deleteThought, 
+    addReaction, 
+    deleteReaction,
+} = require('../../controllers/thought-controller');
+// const getThoughts = require('../../controllers/thought-controller');
 
-const getOneThought = require('../../controllers/thought-controller');
+// const getOneThought = require('../../controllers/thought-controller');
 
-const addThought = require('../../controllers/thought-controller');
+// const addThought = require('../../controllers/thought-controller');
 
-const updateThought = require('../../controllers/thought-controller');
+// const updateThought = require('../../controllers/thought-controller');
 
-const deleteThought = require('../../controllers/thought-controller');
+// const deleteThought = require('../../controllers/thought-controller');
 
 
-const addReaction = require('../../controllers/thought-controller');
-const deleteReaction = require('../../controllers/thought-controller');
+// const addReaction = require('../../controllers/thought-controller');
+// const deleteReaction = require('../../controllers/thought-controller');
 
 router.route("/").get(getThoughts).post(addThought);
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,23 +1,33 @@
 const router = require('express').Router();
 // need to add controllers endpoint to all of these variables 
-const getUsers = require('../../controllers/user-controller');
+const {
+    getUsers, 
+    getOneUser, 
+    addUser, 
+    updateUser, 
+    deleteUser, 
+    addFriend, 
+    deleteFriend,
+} = require('../../controllers/user-controller');
+// const getUsers = require('../../controllers/user-controller');
 
-const getOneUser = require('../../controllers/user-controller');
+// const getOneUser = require('../../controllers/user-controller');
 
-const addUser = require('../../controllers/user-controller');
+// const addUser = require('../../controllers/user-controller');
 
-const updateUser = require('../../controllers/user-controller');
+// const updateUser = require('../../controllers/user-controller');
 
-const deleteUser = require('../../controllers/user-controller');
+// const deleteUser = require('../../controllers/user-controller');
 
 
-const addFriend = require('../../controllers/user-controller');
-const deleteFriend = require('../../controllers/user-controller');
+// const addFriend = require('../../controllers/user-controller');
+// const deleteFriend = require('../../controllers/user-controller');
 
 router.route("/").get(getUsers).post(addUser);
 
-router.route("/:userID").get(getOneUser).put(updateUser);
+router.route("/:userId").get(getOneUser).put(updateUser).delete(deleteUser);
 
 // need route for adding user friends
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
