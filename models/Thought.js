5577424fb33dfecad84b0dafc620860e0d30b0bf
Nunder <nathannunderwood@gmail.com>
2022-11-10 22:11:22 -0800

    forgot reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,9 +1,38 @@
 const { Schema, model } = require('mongoose');
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        }, 
+
+        reactionBody: {
+            type: String, 
+            required: true,
+            maxlength: 300
+        }, 
+
+        username: {
+            type: String, 
+            required: true
+        },
+        // dateCreated
+    }, 
+
+    {
+        toJSON: {
+            getters: true, 
+        },
+        id: false,
+    }
+);
+
 const thoughtSchema = new Schema(
     {
         thoughtBody: {
             type: String, 
+            minlength: 1,
             maxlength: 300
         }, 
         createdAt: {
@@ -25,7 +54,7 @@ const thoughtSchema = new Schema(
     }
 );
 
-thoughtSchema.virtuals("totalReactions").get(() => {
+thoughtSchema.virtual("totalReactions").get(() => {
     return this.reactions.length;
 });
 
